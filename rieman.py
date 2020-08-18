12b213fada7a0697dfccab4b50301f8cbc72ebde
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-08-18 12:31:03 -0700

    PIRATAS

--- rieman.py
+++ rieman.py
@@ -9,15 +9,27 @@ def EULER():
     absolute = abs(math.e)
     error = absolute-suma
     relativeError = error/absolute
-    print("Relative Error:",relativeError)
-    print("Absoluto",absolute)
-    print("Serie:",suma )
-    print('Error:',error)
+    print("Relative Error:", relativeError)
+    print("Absoluto", absolute)
+    print("Serie:", suma)
+    print('Error:', error)
+
 
 def LOGARTIMO():
-
     print('\n_________\nLOGARITMO')
 
+def SENOR_PIG():
+    serie = float()
+    #ciclos = int(input("Ciclos: "))
+    odd = 1
+    for k in range(0, 400000):
+        serie += (((-1)**(k+1)))/((2*k)+1)
 
-EULER()
-LOGARTIMO()
+    pi = float(serie*4)
+    print('Pi:', pi)
+    print('Con arco tangente:', math.atan(1)*4)
+    print("Absoluto con <math.pi> :",abs(math.pi))
+
+
+#EULER()
+SENOR_PIG()
